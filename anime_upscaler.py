4a1bc053724759e6c940909d31ea8fa277b44743
shangar <sharanshangar@gmail.com>
2022-01-13 19:58:45 -0500

    added support to split images and join before upscaling

--- anime_upscaler.py
+++ anime_upscaler.py
@@ -5,6 +5,23 @@ from tqdm import tqdm
 import os
 import argparse
 import shutil
+import image_slicer
+from image_slicer import join
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--model_path', type=str, help='REQUIRED: specify path of the model being used')
+parser.add_argument('-i', '--input', type=str, help='REQUIRED: specify path of the image you want to upscale')
+parser.add_argument('-o', '--output', type=str, help='REQUIRED: specify path where you want to save image')
+parser.add_argument('-s', '--slice', nargs='?', type=int, const=4, help='OPTIONAL: specify weather to split frames, recommended to use to help with VRAM unless you got a fucken quadro or something' )
+parser.add_argument('-a', '--audio', action='store_true', help='OPTIONAL: specify weather you want to copy audio from source as well')
+parser.add_argument('-c', '--clear_temp', action='store_true', help='OPTIONAL: specify weather you want to clear temporary folder with upscaled frames after you are finished with final video')
+parser.add_argument('-e', '--extract_audio', type=str, help='OPTIONAL: specify weather you want to seperately save the audio file from video (used if you want to use the audio with a seperate program like ffmpeg)')
+args = parser.parse_args()
 
 def extract_frames(vid_path):
 	vid = cv2.VideoCapture(vid_path)
@@ -23,35 +40,42 @@ def create_temp_folder(vid_path):
 		os.mkdir('tmp')
 		create_temp_folder(vid_path)
 
-def clear_temp(temp_path):
-	shutil.rmtree(temp_path)
+def get_dir(path):
+	if not os.path.exists(path):
+		os.mkdir(path)
+	return path
 
-def setup_frames(vid_path):
+def setup_frames(vid_path, slice=None):
 	folder_name = vid_path.split('/')[-1].split('.')[0]
 	images = extract_frames(vid_path)
 	create_temp_folder(vid_path)
 	os.mkdir('tmp/{}/original'.format(folder_name))
+	slices = []
 	for i in tqdm(range(len(images))):
-		cv2.imwrite('tmp/{}/original'.format(folder_name)+'/frame_{}.jpg'.format(i), images[i])
+		cv2.imwrite('tmp/{}/original'.format(folder_name)+'/frame_{}.png'.format(i), images[i])
 	os.mkdir('tmp/{}/upscaled'.format(folder_name))
 
-def upscale(vid_path):
+def upscale(vid_path, slice=None):
 	folder_name = vid_path.split('/')[-1].split('.')[0]
 	print('extracting frames...')
-	setup_frames(vid_path)		
+	setup_frames(vid_path)
 	print('upscaling...')
 	for i in tqdm(os.listdir('tmp/{}/original'.format(folder_name))):
-		out = frame_esrgan.upscale(args.model_path, 'tmp/{}/original/{}'.format(folder_name, i))
-		cv2.imwrite('tmp/{}/upscaled/{}'.format(folder_name, i), out)
+		if slice:
+			out = frame_esrgan.upscale_slice(args.model_path,  'tmp/{}/original/{}'.format(folder_name, i), slice)
+			out.save('tmp/{}/upscaled/{}'.format(folder_name, i))
+		else:
+			out = frame_esrgan.upscale(args.model_path, 'tmp/{}/original/{}'.format(folder_name, i))
+			cv2.imwrite('tmp/{}/upscaled/{}'.format(folder_name, i), out)
 
 def combine_frames(video_path, new_video_path):
 	folder_name = video_path.split('/')[-1].split('.')[0]
 	images = [img for img in os.listdir('tmp/{}/upscaled'.format(folder_name))]
-	height, width, layers = cv2.imread('tmp/{}/upscaled/frame_0.jpg'.format(folder_name)).shape
+	height, width, layers = cv2.imread('tmp/{}/upscaled/frame_0.png'.format(folder_name)).shape
 	fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 	video = cv2.VideoWriter(new_video_path, fourcc, 30, (width, height))
 	for i in tqdm(range(len(images))):
-		video.write(cv2.imread('tmp/{}/upscaled/frame_{}.jpg'.format(folder_name, i)))
+		video.write(cv2.imread('tmp/{}/upscaled/frame_{}.png'.format(folder_name, i)))
 	cv2.destroyAllWindows()
 	video.release()
 
@@ -66,13 +90,17 @@ def copy_audio(original_video_path, new_video_path):
 	final_clip = new_clip.set_audio(clip.audio)
 	final_clip.write_videofile(new_video_path)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('model_path', type=str, help='REQUIRED: specify path of the model being used')
-args = parser.parse_args()
-
-extract_audio('test_img/test_clip.mp4', 'test_img/test_clip.mp3')
-# copy_audio('test_img/test_clip.mp4', 'test_img/test_clip_upscaled_copy.mp4')
-# combine_frames('test_img/test_clip.mp4', 'test_img/test_clip_upscaled_copy.mp4')
-# audio = upscale('test_img/test_clip.mp4')
-# clear_temp('tmp/test_clip')
-# 'test_img/test_clip_upscaled.mp4'
+if __name__ == '__main__':
+	if args.model_path and args.input and args.output:
+		try:
+			upscale(args.input, slice=args.slice)
+			combine_frames(args.input, args.output)
+			if args.audio:
+				copy_audio(args.input, args.output)
+			if args.clear_temp:
+				shutil.rmtree('tmp')
+			if args.extract_audio:
+				extract_audio(args.input, args.extract_audio)
+		except Exception as e:
+			print(e)
+			shutil.rmtree('tmp')
--- frame_esrgan.py
+++ frame_esrgan.py
@@ -3,14 +3,13 @@ import torchvision
 from basicsr.archs.rrdbnet_arch import RRDBNet
 from realesrgan import RealESRGANer
 import cv2
-
-# These names are kinda random, because in python you can't have a variable name start with a number
-# CINEMA is basically 4K
-# DESKTOP is 1440p
-# TV is 1080p
-CINEMA = (3840, 2160)
-DESKTOP = (2560, 1440)
-TV = (1920, 1080)
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import image_slicer
+from image_slicer import join
+from PIL import Image
+import numpy as np
 
 def upscale(model_path, im_path):
 	model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
@@ -19,7 +18,47 @@ def upscale(model_path, im_path):
 	output, _ = upsampler.enhance(img, outscale=4)
 	return output
 
-model_path = '/home/ssm/Documents/Real-ESRGAN/experiments/pretrained_models/RealESRGAN_x4plus_anime_6B.pth'
-print('upscaling...')
-output = upscale(model_path,'/home/ssm/Downloads/wp2172808-great-teacher-onizuka-wallpapers.jpg')
-cv2.imwrite('test_img/upscaled_wp.jpg', output)
+
+def upscale_slice(model_path, image, slice):
+	width, height = Image.open(image).size
+	tiles = image_slicer.slice(image, slice, save=False)
+	model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
+	upsampler = RealESRGANer(scale=4, model_path=model_path, model=model, tile=0, tile_pad=10, pre_pad=0, half=False)
+	for tile in tiles:
+		output, _ = upsampler.enhance(np.array(tile.image), outscale=4)
+		tile.image = Image.fromarray(output)
+		tile.coords = (tile.coords[0]*4, tile.coords[1]*4)
+	return join(tiles, width=width*4, height=height*4)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-m', '--model_path', type=str, help='REQUIRED: specify path of the model being used')
+	parser.add_argument('-i', '--input', type=str, help='REQUIRED: specify path of the image you want to upscale')
+	parser.add_argument('-o', '--output', type=str, help='REQUIRED: specify path where you want to save image')
+	parser.add_argument('-v', '--visualize', action='store_true', help='OPTIONAL: add this to see how image looks before and after upscale')
+	parser.add_argument('-s', '--slice', nargs='?', type=int, const=4, help='OPTIONAL: specify weather to split frames, recommended to use to help with VRAM unless you got a fucken quadro or something' )
+	args = parser.parse_args()
+
+	if args.model_path and args.input and args.output:
+		if args.slice:
+			output = upscale_slice(args.model_path, args.input, args.slice)
+			output.save(args.output)
+		else:
+			output = upscale(args.model_path, args.input)
+			cv2.imwrite(args.output, output)
+		if args.visualize:
+			plt.imshow(mpimg.imread(args.input))
+			plt.show()
+			plt.imshow(output)
+			plt.show()
+	else:
+		print('Error: Missing arguments, check -h, --help for details')
+
+
+			# tiles = image_slicer.slice('tmp/{}/original/{}'.format(folder_name, i), slice, save=False)
+			# print(tiles)
+			# for tile in tiles:
+			# 	up = frame_esrgan.upscale_slice(args.model_path, np.array(tile.image))
+			# 	tile.image = Image.fromarray(up, 'RGB')
+			# out = join(tiles)
+			# out.save('tmp/{}/upscaled/{}'.format(folder_name, i.replace('jpg', 'png')))
