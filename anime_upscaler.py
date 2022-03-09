a7706092be305c98f0622984f8d01bd8ae4572e3
Markus Törnqvist <mjt@nysv.org>
2022-03-09 19:20:09 +0200

    Define save prefix and be able to reuse frames

--- anime_upscaler.py
+++ anime_upscaler.py
@@ -17,23 +17,51 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model_path', type=str, help='REQUIRED: specify path of the model being used')
 parser.add_argument('-i', '--input', type=str, help='REQUIRED: specify path of the image you want to upscale')
 parser.add_argument('-o', '--output', type=str, help='REQUIRED: specify path where you want to save image')
+parser.add_argument('-S', '--save-prefix', type=str, help='OPTIONAL: Save frames with prefix to save memory')
 parser.add_argument('-s', '--slice', nargs='?', type=int, const=4, help='OPTIONAL: specify weather to split frames, recommended to use to help with VRAM unless you got a fucken quadro or something' )
 parser.add_argument('-a', '--audio', action='store_true', help='OPTIONAL: specify weather you want to copy audio from source as well')
 parser.add_argument('-c', '--clear_temp', action='store_true', help='OPTIONAL: specify weather you want to clear temporary folder with upscaled frames after you are finished with final video')
 parser.add_argument('-e', '--extract_audio', type=str, help='OPTIONAL: specify weather you want to seperately save the audio file from video (used if you want to use the audio with a seperate program like ffmpeg)')
 args = parser.parse_args()
 
-def extract_frames(vid_path, save=''):
+def extract_frameno(fname):
+	no_ext = fname.rsplit('.', 1)[0]
+	only_num = no_ext.rsplit('_', 1)[1]
+
+	return int(only_num)
+
+def extract_frames(vid_path, save_prefix=''):
+	save = '{}_{{}}.png'.format(save_prefix) if save_prefix else False
+
+	if not os.path.exists(vid_path):
+		raise RuntimeError(f'path missing {vid_path}')
+
+	folder_name = vid_path.split('/')[-1].split('.')[0]
+	original = os.path.join('tmp', folder_name, 'original')
+
+	# Reuse frames if we have them
+	if save_prefix:
+		files = os.listdir(original)
+		files = (f for f in files if f.startswith(save_prefix) and '_' in f and f.endswith('.png'))
+		files = sorted(files, key=extract_frameno)
+		if files:
+			print(f'Found {len(files)} frames extracted')
+			return files
+
 	vid = cv2.VideoCapture(vid_path)
 	images = []
 	count = 0
+
 	success, image = vid.read()
 	while success:
 		if not save:
 			images.append(image)
 		else:
 			print('saving frame {}...'.format(count))
-			cv2.imwrite(save.format(count), image)
+			fname = save.format(count)
+			fpath = os.path.join(original, fname)
+			cv2.imwrite(fpath, image)
+			images.append(fname)
 			print('done saving frame {}...'.format(count))
 		success, image = vid.read()
 		count += 1
@@ -46,7 +74,9 @@ def get_fps(vid_path):
 def create_temp_folder(vid_path):
 	if os.path.exists('tmp'):
 		folder_name = vid_path.split('/')[-1].split('.')[0]
-		os.mkdir('tmp/{}'.format(folder_name))
+		tmp_path = os.path.join('tmp', folder_name)
+		if not os.path.exists(tmp_path):
+			os.mkdir(tmp_path)
 	else:
 		os.mkdir('tmp')
 		create_temp_folder(vid_path)
@@ -56,38 +86,71 @@ def get_dir(path):
 		os.mkdir(path)
 	return path
 
-def setup_frames(vid_path, slice=None):
-	folder_name = vid_path.split('/')[-1].split('.')[0]
-	images = extract_frames(vid_path)
+def setup_frames(vid_path, slice=None, save_prefix=''):
 	create_temp_folder(vid_path)
-	os.mkdir('tmp/{}/original'.format(folder_name))
-	slices = []
-	for i in tqdm(range(len(images))):
-		cv2.imwrite('tmp/{}/original'.format(folder_name)+'/frame_{}.png'.format(i), images[i])
-	os.mkdir('tmp/{}/upscaled'.format(folder_name))
 
-def upscale(vid_path, slice=None):
 	folder_name = vid_path.split('/')[-1].split('.')[0]
-	print('extracting frames...')
-	setup_frames(vid_path)
-	print('upscaling...')
-	for i in tqdm(os.listdir('tmp/{}/original'.format(folder_name))):
-		if slice:
-			out = frame_esrgan.upscale_slice(args.model_path,  'tmp/{}/original/{}'.format(folder_name, i), slice)
-			out.save('tmp/{}/upscaled/{}'.format(folder_name, i))
-		else:
-			out = frame_esrgan.upscale(args.model_path, 'tmp/{}/original/{}'.format(folder_name, i))
-			cv2.imwrite('tmp/{}/upscaled/{}'.format(folder_name, i), out)
 
-def combine_frames(video_path, new_video_path):
+	original = os.path.join('tmp', folder_name, 'original')
+	upscaled = os.path.join('tmp', folder_name, 'upscaled')
+
+	if not os.path.exists(original):
+		os.mkdir(original)
+	if not os.path.exists(upscaled):
+		os.mkdir(upscaled)
+
+	images = extract_frames(vid_path, save_prefix=save_prefix)
+	slices = []
+	if not save_prefix:
+		for i in tqdm(range(len(images))):
+			fname = 'frame_{}.png'.format(i)
+			ipath = os.path.join(original, fname)
+			cv2.imwrite(ipath, images[i])
+
+def upscale(vid_path, slice=None, save_prefix=''):
+	folder_name = vid_path.split('/')[-1].split('.')[0]
+
+	original = os.path.join('tmp', folder_name, 'original')
+	upscaled = os.path.join('tmp', folder_name, 'upscaled')
+
+	print('extracting frames...')
+	setup_frames(vid_path, save_prefix=save_prefix)
+
+	print('upscaling...')
+	for i in tqdm(os.listdir(original)):
+		original_f = os.path.join(original, i)
+		upscaled_f = os.path.join(upscaled, i)
+
+		if slice:
+			out = frame_esrgan.upscale_slice(args.model_path, original_f, slice)
+			out.save(upscaled_f)
+		else:
+			out = frame_esrgan.upscale(args.model_path, original_f)
+			cv2.imwrite(upscaled_f, out)
+
+def combine_frames(video_path, new_video_path, save_prefix=''):
 	folder_name = video_path.split('/')[-1].split('.')[0]
-	images = [img for img in os.listdir('tmp/{}/upscaled'.format(folder_name))]
-	height, width, layers = cv2.imread('tmp/{}/upscaled/frame_0.png'.format(folder_name)).shape
+
+	original = os.path.join('tmp', folder_name, 'original')
+	upscaled = os.path.join('tmp', folder_name, 'upscaled')
+
+	if not save_prefix:
+		save_prefix = 'frame'
+
+	images = [img for img in os.listdir(upscaled)]
+	file0 = os.path.join(upscaled, '{}_0.png'.format(save_prefix))
+	height, width, layers = cv2.imread(file0).shape
+
 	fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+
 	fps = get_fps(video_path)
 	video = cv2.VideoWriter(new_video_path, fourcc, fps, (width, height))
+
 	for i in tqdm(range(len(images))):
-		video.write(cv2.imread('tmp/{}/upscaled/frame_{}.png'.format(folder_name, i)))
+		fname = '{}_{}.png'.format(save_prefix, i)
+		fpath = os.path.join(upscaled, fname)
+		video.write(cv2.imread(fpath))
+
 	cv2.destroyAllWindows()
 	video.release()
 
@@ -105,7 +168,7 @@ def copy_audio(original_video_path, new_video_path, new_name=''):
 if __name__ == '__main__':
 	if args.model_path and args.input and args.output:
 		try:
-			upscale(args.input, slice=args.slice)
+			upscale(args.input, slice=args.slice, save_prefix=args.save_prefix)
 			combine_frames(args.input, args.output)
 			if args.audio:
 				copy_audio(args.input, args.output)
