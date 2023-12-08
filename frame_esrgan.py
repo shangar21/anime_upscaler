b461c6bd5831b006a3c25a437d15ec014073f106
shangar <sharanshangar@gmail.com>
2023-12-08 04:09:26 +0000

    Now resizes image after upscaling if option is selected

--- frame_esrgan.py
+++ frame_esrgan.py
@@ -12,6 +12,9 @@ from PIL import Image
 import numpy as np
 from tqdm import tqdm
 
+def convert_from_image_to_cv2(img: Image) -> np.ndarray:
+	# return np.asarray(img)
+	return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
 def upscale(model_path, im_path):
 	model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
@@ -29,7 +32,7 @@ def upscale_slice(model_path, image, slice):
 		output, _ = upsampler.enhance(np.array(tile.image), outscale=4)
 		tile.image = Image.fromarray(output)
 		tile.coords = (tile.coords[0]*4, tile.coords[1]*4)
-	return join(tiles, width=width*4, height=height*4)
+	return convert_from_image_to_cv2(join(tiles, width=width*4, height=height*4))
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -37,22 +40,25 @@ if __name__ == '__main__':
 	parser.add_argument('-i', '--input', type=str, help='REQUIRED: specify path of the image you want to upscale')
 	parser.add_argument('-o', '--output', type=str, help='REQUIRED: specify path where you want to save image')
 	parser.add_argument('-v', '--visualize', action='store_true', help='OPTIONAL: add this to see how image looks before and after upscale')
-	parser.add_argument('-s', '--slice', nargs='?', type=int, const=4, help='OPTIONAL: specify weather to split frames, recommended to use to help with VRAM unless you got a fucken quadro or something' )
+	parser.add_argument('-s', '--slice', nargs='?', type=int, const=4, help='OPTIONAL: specify weather to split frames, recommended to use to help with VRAM unless you got a fucken quadro or something')
+	parser.add_argument('-r', '--resize', nargs='?', type=str, const='1920x1080', help="OPTIONAL: specify whether to resize image to a specific resolution. Specify with widthxheight, for example 1920x1080")
 	args = parser.parse_args()
+
 
 	if args.model_path and args.input and args.output:
 		if args.slice:
 			output = upscale_slice(args.model_path, args.input, args.slice)
-			print(output.size)
-			output.save(args.output)
 		else:
 			output = upscale(args.model_path, args.input)
-			cv2.imwrite(args.output, output)
 		if args.visualize:
 			plt.imshow(mpimg.imread(args.input))
 			plt.show()
 			plt.imshow(output)
 			plt.show()
+		if args.resize:
+			size = tuple(int(i) for i in args.resize.split('x'))
+			output = cv2.resize(output, size)
+		cv2.imwrite(args.output, output)
 	else:
 		print('Error: Missing arguments, check -h, --help for details')
 
